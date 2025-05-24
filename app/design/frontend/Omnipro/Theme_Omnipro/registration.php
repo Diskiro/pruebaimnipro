6fe6842dc5419d160996fb7fb60f8cddcf386631
Joshua <jam0624@hotmail.com>
2025-05-24 11:26:32 -0600

    salvar avance

---
+++ app/design/frontend/Omnipro/Theme_Omnipro/registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Copyright © Omnipro All rights reserved.
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::THEME,
+    'frontend/Omnipro/Theme_Omnipro',
+    __DIR__
+); 
